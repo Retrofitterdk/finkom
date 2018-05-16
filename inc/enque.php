4211d3349b42f545fd6ae02df363e7a8441a3ab2
sbangnielsen <sbn@dagensmedicin.dk>
2018-05-16 13:46:01 +0200

    Move js to assets folder

--- inc/enque.php
+++ inc/enque.php
@@ -7,9 +7,9 @@ function finkom_scripts() {
 
   wp_enqueue_style( 'finkom-fonts', get_template_directory_uri() . '/assets/fonts/fonts.css', array(), '1.0.0' );
 
-	wp_enqueue_script( 'finkom-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'finkom-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'finkom-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'finkom-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
