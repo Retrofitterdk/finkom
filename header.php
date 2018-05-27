d2860846a3a2c4f39276b158375bde6315c0607b
sbangnielsen <sbn@dagensmedicin.dk>
2018-05-27 13:25:13 +0200

    Make non-js menu

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Fin:kom
- */
+* The header for our theme
+*
+* This is the template that displays all of the <head> section and everything up until <div id="content">
+*
+* @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+*
+* @package Fin:kom
+*/
 
 ?>
 <!doctype html>
@@ -21,39 +21,42 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'finkom' ); ?></a>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'finkom' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			$site_title = get_bloginfo( 'name' );
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php finkom_split_title($site_title); ?></a></h1>
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php finkom_split_title($site_title); ?></a></p>
-				<?php
-			endif;
-			$finkom_description = get_bloginfo( 'description', 'display' );
-			if ( $finkom_description || is_customize_preview() ) :
-				?>
-				<p class="site-description screen-reader-text"><?php echo $finkom_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				the_custom_logo();
+				$site_title = get_bloginfo( 'name' );
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php finkom_split_title($site_title); ?></a></h1>
+					<?php
+					else :
+						?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php finkom_split_title($site_title); ?></a></p>
+						<?php
+					endif;
+					$finkom_description = get_bloginfo( 'description', 'display' );
+					if ( $finkom_description || is_customize_preview() ) :
+						?>
+						<p class="site-description screen-reader-text"><?php echo $finkom_description; /* WPCS: xss ok. */ ?></p>
+					<?php endif; ?>
+				</div><!-- .site-branding -->
+				<a href="#site-navigation" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Open primary menu', 'finkom' ); ?>" aria-expanded="false"><?php esc_html_e( 'Open primary Menu', 'finkom' ); ?></a>
+				<nav id="site-navigation" class="main-navigation" aria-expanded="false">
+					<a href="#" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Close primary menu', 'finkom' ); ?>" aria-expanded="false"><?php esc_html_e( 'Close primary Menu', 'finkom' ); ?></a>
+					<?php
+					wp_nav_menu( array(
+						'theme_location'  => 'menu-1',
+						'menu_id'         => 'primary-menu',
+						'container_class' => 'nav-menu',
+						'container_id'    => 'primary-menu-container',
+					) );
+					?>
+				</nav><!-- #site-navigation -->
+				<a href="#" class="backdrop" tabindex="-1" aria-hidden="true" hidden></a>
+			</header><!-- #masthead -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'finkom' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+			<div id="content" class="site-content">
