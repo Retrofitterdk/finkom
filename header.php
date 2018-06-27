b8ba5f90f76c3b717f02b2df51c2dbc73d608f70
sbangnielsen <sbn@dagensmedicin.dk>
2018-06-27 12:05:14 +0200

    Add header image template part

--- header.php
+++ header.php
@@ -25,34 +25,7 @@
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'finkom' ); ?></a>
 
 		<header id="masthead" class="site-header">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				$site_title = get_bloginfo( 'name' );
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php finkom_split_title($site_title); ?></a></h1>
-					<?php
-					else :
-						?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php finkom_split_title($site_title); ?></a></p>
-						<?php
-					endif;
-					$finkom_description = get_bloginfo( 'description', 'display' );
-					if ( $finkom_description || is_customize_preview() ) :
-						?>
-						<p class="site-description screen-reader-text"><?php echo $finkom_description; /* WPCS: xss ok. */ ?></p>
-					<?php endif; ?>
-				</div><!-- .site-branding -->
-				<a href="#site-navigation" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Open primary menu', 'finkom' ); ?>" aria-expanded="false"><?php esc_html_e( 'Content', 'finkom' ); ?></a>
-				<nav id="site-navigation" class="main-navigation" aria-expanded="false">
-					<a href="#" role="button" id="primary-menu-toggle" class="menu-toggle" aria-controls="primary-menu" aria-label="<?php esc_html_e( 'Close primary menu', 'finkom' ); ?>" aria-expanded="false"><?php esc_html_e( 'Content', 'finkom' ); ?></a>
-					<?php
-					get_template_part( 'components/navigation/menu', 'main' );
-					get_template_part( 'components/navigation/menu', 'social' );
-					?>
-				</nav><!-- #site-navigation -->
-				<a href="#" class="backdrop" tabindex="-1" aria-hidden="true" hidden></a>
+			<?php get_template_part( 'components/header/header', 'image' ); ?>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
