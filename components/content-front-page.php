d4e617304e509809fb6dd5a5088682e8c3468811
sbangnielsen <sbn@dagensmedicin.dk>
2018-10-28 10:42:17 +0100

    Remove wrapper
    Remove article wrapper on front-page

--- components/content-front-page.php
+++ components/content-front-page.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * Template part for displaying page content in front-page.php
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Fin:kom
- */
+* Template part for displaying page content in front-page.php
+*
+* @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+*
+* @package Fin:kom
+*/
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<?php
-		get_template_part( 'components/media/entrymedia', 'page' );
-		get_template_part( 'components/content/entrycontent', 'page' );
-		?>
-</article><!-- #post-<?php the_ID(); ?> -->
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+	get_template_part( 'components/media/entrymedia', 'page' );
+	get_template_part( 'components/content/entrycontent', 'page' );
+	?>
+</div>
+<!-- #post-<?php the_ID(); ?> -->
